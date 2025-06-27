6e3161a220dc39ff079d269150d8b379d0e9075f
jiseong1688 <wltjd1688@naver.com>
2025-06-27 12:15:06 +0900

    build: 스크립트 수정 feat: 서버 health check 구현

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3002;
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ message: "Hello I'm auth service" });
 });
 
+app.get('/health', (_req, res) => res.status(200).json({ status: 'ok', service: 'auth-service', timestamp: new Date().toISOString() }));
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
--- apps/public-service/src/main.ts
+++ apps/public-service/src/main.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3006;
 
 const app = express();
-
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ message: "Hello I'm public service" });
 });
+
+app.get('/health', (_req, res) => res.status(200).json({ status: 'ok', service: 'public-service', timestamp: new Date().toISOString() }));
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
