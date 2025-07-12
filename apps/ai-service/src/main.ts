51950476a7187594165cf3056137fe28b5e89aa5
jiseong1688 <wltjd1688@naver.com>
2025-07-12 18:13:04 +0900

    Revert "Revert "Ai service""

--- apps/ai-service/src/main.ts
+++ apps/ai-service/src/main.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
 import AiChat from './routes/aichat.js';
 import { serviceConfig, headerToLocals } from '@shared/config';
 import dotenv from 'dotenv';
