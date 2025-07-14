4339fee035f69727d65c2e74cdb04557326cc54b
jiseong1688 <wltjd1688@naver.com>
2025-07-14 17:57:05 +0900

    hotfix: 타입단언 수정

--- apps/interaction-service/src/modules/ensureAuthorization.ts
+++ apps/interaction-service/src/modules/ensureAuthorization.ts
@@ -16,7 +16,8 @@ export const ensureAuthorization = (req: Request): DecodedJwt | Error => {
     const token = req.header('x-access-token');
 
     if (token) {
-      const decodedJwt = jwt.verify(token, process.env.PRIVATE_KEY as string) as DecodedJwt;
+      const secretOrKey = process.env.JWT_PUBLIC_KEY || process.env.PRIVATE_KEY!;
+      const decodedRaw = jwt.verify(token, secretOrKey);
       return decodedJwt;
     } else {
       throw new ReferenceError('JWT must be provided');
